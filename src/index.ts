29a532fc5f12fc85cdf16c6f7b333097cc19a459
Aaron Adrian <aaroncadrian@gmail.com>
2020-09-19 00:13:52 -0500

    Add figlet text

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const path = require('path');
+const program = require('commander');
+
+clear();
+
+console.log(figlet.textSync('pizza-cli', { horizontalLayout: 'full' }))
