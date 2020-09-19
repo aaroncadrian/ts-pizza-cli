1aa24c2be6cc47ac72b7518c43e16d7d7da49ef7
Aaron Adrian <aaroncadrian@gmail.com>
2020-09-19 00:19:57 -0500

    Add options

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,14 @@ const program = require('commander');
 
 clear();
 
-console.log(figlet.textSync('pizza-cli', { horizontalLayout: 'full' }))
+console.log(figlet.textSync('pizza-cli', { horizontalLayout: 'full' }));
+
+program
+  .version('0.0.1')
+  .description('An example CLI for ordering pizza')
+  .option('-p', '--peppers', 'Add peppers')
+  .option('-P', '--pineapple', 'Add pineapple')
+  .option('-b', '--bbq', 'Add BBQ sauce')
+  .option('-c', '--cheese <type>', 'Add the specified type of cheese')
+  .option('-C', '--no-cheese', 'You do not want any cheese')
+  .parse(program.argv);
